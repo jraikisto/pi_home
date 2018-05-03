cc159252e2062651ea82687182c2487232e2f79a
jraikisto <Raikisto.Jaakko.N@student.uta.fi>
2018-05-04 01:02:53 +0300

    added lamp

---
+++ python/lamp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from time import sleep
+import unicornhat as unicorn
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(180)
+unicorn.brightness(0.80)
+uh_width,uh_height=unicorn.get_shape()
+
+print("Your unicornhat will now light up!")
+
+for h in range(uh_height):
+    for w in range(uh_width):
+        unicorn.set_pixel(w, h, 255, 0, 0)
+
+unicorn.show()
+sleep(3600)
